585e8c6fbd22ace6e996976d8d320395d6add446
roi <roi890@gmail.com>
2016-06-17 12:42:44 +0300

    rrrr

--- app/src/main/java/com/example/tomer/ex4/SplashActivity.java
+++ app/src/main/java/com/example/tomer/ex4/SplashActivity.java
@@ -1,7 +1,12 @@
 package com.example.tomer.ex4;
 
-import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.RadialGradient;
+import android.graphics.Shader;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.RectShape;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,24 +14,53 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
-import android.widget.Toast;
+import android.widget.RelativeLayout;
 
 import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class SplashActivity extends AppCompatActivity {
 
     int counter;
     ProgressBar progressBar;
     Random rand = new Random();
+    RelativeLayout rl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        rl = (RelativeLayout) findViewById(R.id.main_activity);
+
+        Timer timer = new Timer();
+        MyTimer mt = new MyTimer();
+        timer.schedule(mt, 50, 50);
         HideActionBar();
         this.progressBar = (ProgressBar)findViewById(R.id.bar);
-        //this.counter = 0;
         startProgressBar();
+    }
+
+
+    class MyTimer extends TimerTask {
+
+        float[] colors1 = {250, 40, 100};
+        float[] colors2 = {120, 40, 100};
+
+        public void run() {
+            runOnUiThread(new Runnable() {
+
+                public void run() {
+                    colors1[0] = (colors1[0] + 1) % 360;
+                    colors2[0] = (colors2[0] + 1) % 360;
+                    ShapeDrawable mDrawable = new ShapeDrawable(new RectShape());
+                    mDrawable.getPaint().setShader(new LinearGradient( 0, 0, 0, rl.getHeight(), Color.HSVToColor(colors1),
+                            Color.HSVToColor(colors2), Shader.TileMode.CLAMP));
+                    rl.setBackgroundDrawable(mDrawable);
+
+                }
+            });
+        }
     }
 
     private void HideActionBar(){
@@ -37,6 +71,8 @@ public class SplashActivity extends AppCompatActivity {
 
 
     }
+
+
 
     private void startProgressBar(){
         Thread t = new Thread(new Runnable() {
@@ -62,7 +98,7 @@ public class SplashActivity extends AppCompatActivity {
                 }//end of loop
 
 
-                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                Intent intent = new Intent(SplashActivity.this, LoginRegisterTabs.class);
                 startActivity(intent);
             }
         });
